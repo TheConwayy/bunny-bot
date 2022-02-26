1e6c98fa39edd0430b188818398dafec037f6fb9
Devin Conway <devinthomas1208@gmail.com>
2022-02-26 14:44:00 -0500

    added a leave event that isn't working (fixing at a later date)

--- index.ts
+++ index.ts
@@ -45,7 +45,9 @@ Client.on('guildMemberAdd', (member) => {
             .replace('{member.user}', `${member}`)  
             .replace('{member.username}', `${member.user.username}`)
             .replace('{member.hashtag}', `${member.user.discriminator}`)
-            .replace('{server}', `${member.guild.name}`);
+            .replace('{server}', `${member.guild.name}`)
+            .replace('{member.count}', `${member.guild.memberCount}`)
+            .replace('{joined}', `<t:${Math.floor(Date.parse(new Date().toString()) / 1000)}:R>`)
 
         function embedThumb() {
             if (embedconfig.image === 'avatar') {
@@ -67,7 +69,7 @@ Client.on('guildMemberAdd', (member) => {
 
         const wEmbed = {
             author: {
-                name: `${member.user.username}#${member.user.discriminator} Joined!`,
+                name: `${member.user.username}#${member.user.discriminator} joined!`,
                 icon_url: `${member.user.displayAvatarURL({ format: 'png' })}`
             },
             description: `${BDescription}`,
@@ -80,6 +82,7 @@ Client.on('guildMemberAdd', (member) => {
 
         if (wChannel?.type === 'GUILD_TEXT') wChannel.type === 'GUILD_TEXT'
         else {
+
             console.log('\x1b[31m%s\x1b[m', '[Bunny Bot] :: Welcome channel must be a text channel')
             return
         }
@@ -88,6 +91,104 @@ Client.on('guildMemberAdd', (member) => {
             content: `Welcome to the server ${member}`,
             embeds: [wEmbed]
         });
+
+    } else {
+
+        const wChannel = member.guild.channels.cache.get(config.welcome.channel)
+        
+        if (wChannel?.type === 'GUILD_TEXT') wChannel.type === 'GUILD_TEXT'
+        else {
+
+            console.log('\x1b[31m%s\x1b[m', '[Bunny Bot] :: Welcome channel must be a text channel')
+            return
+
+        }
+
+        const messageContent = config.welcome.message
+
+        wChannel.send({
+            content: `${messageContent}`
+        })
+
+    }
+
+})
+
+Client.on('guildMemberRemove', (member) => {
+
+    if (config.left.embed) {
+
+        const embedconfig = embeds.left
+        const lChannel = member.guild.channels.cache.get(config.left.channel)
+
+        let description = embeds.left.description
+        let BDescription = description
+            .replace('{member.user}', `${member}`)  
+            .replace('{member.username}', `${member.user.username}`)
+            .replace('{member.hashtag}', `${member.user.discriminator}`)
+            .replace('{member.count}', `${member.guild.memberCount}`)
+            .replace('{joined}', `<t:${Math.floor(Date.parse(new Date().toString()) / 1000)}:R>`)
+
+        function embedThumb() {
+            if (embedconfig.image === 'avatar') {
+                let avatar = member.user.displayAvatarURL({ format: 'png' })
+                return avatar
+            } else if (embedconfig.image === 'server') {
+                let guild = member.guild
+                let image = ''
+
+                if (guild.icon) {
+                    image = `${guild.iconURL({ format: 'png' })}`
+                    return image
+                } else {
+                    image = `${embedconfig.imageURL}`
+                    return image
+                }
+            }
+        }
+
+        const lEmbed = {
+            author: {
+                name: `${member.user.username}#${member.user.discriminator} left!`,
+                icon_url: `${member.user.displayAvatarURL({ format: 'png' })}`
+            },
+            description: `${BDescription}`,
+            footer: {
+                text: `${embedconfig.footer}`
+            },
+            thumbnail: { url: `${embedThumb()}` },
+            timestamp: new Date()
+        }
+
+        if (lChannel?.type === 'GUILD_TEXT') lChannel.type === 'GUILD_TEXT'
+        else {
+
+            console.log('\x1b[31m%s\x1b[m', '[Bunny Bot] :: "left" channel must be a text channel')
+            return
+        }
+
+        lChannel.send({
+            content: `${member} has left the server!`,
+            embeds: [lEmbed]
+        });
+
+    } else {
+
+        const lChannel = member.guild.channels.cache.get(config.left.channel)
+        
+        if (lChannel?.type === 'GUILD_TEXT') lChannel.type === 'GUILD_TEXT'
+        else {
+
+            console.log('\x1b[31m%s\x1b[m', '[Bunny Bot] :: "left" channel must be a text channel')
+            return
+
+        }
+
+        const messageContent = config.left.message
+
+        lChannel.send({
+            content: `${messageContent}`
+        })
 
     }
 
